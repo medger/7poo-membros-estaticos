43c8d15cdfab50bb9f128e56e4a29cd655df9a16
Medina <dnt.1554@gmail.com>
2023-04-25 18:01:17 -0300

    Apenas alguns exemplos

---
+++ esfera_util/Calculator.java
@@ -0,0 +1,14 @@
+package esfera_util;
+
+public class Calculator {
+	public final double PI = 3.14159; //final = constante. Sempre em maísculo;
+	
+	public double circumference(double radius) {
+		return 2 * PI * radius;
+	}
+	
+	public double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
+	
+}
